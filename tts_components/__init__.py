c7ec5f6ad4355c37c909243f62f9341c2e809fce
Weiyu <wwang1110@gmail.com>
2025-09-24 09:51:10 -0700

    enable cuda

--- tts_components/__init__.py
+++ tts_components/__init__.py
@@ -1,12 +1,9 @@
 from .config import Config
-from .models import TTSRequest, BatchTTSRequest
 from .utils import simple_smart_split, batch_split
 from .integrated_g2p import G2PItem, IntegratedG2P
 
 __all__ = [
     "Config",
-    "TTSRequest",
-    "BatchTTSRequest",
     "simple_smart_split",
     "batch_split",
     "G2PItem",
--- tts_components/config.py
+++ tts_components/config.py
@@ -1,10 +1,7 @@
 import os
 
 class Config:
-    """Service configuration"""
     def __init__(self):
         self.kokoro_fp16 = os.getenv("KOKORO_FP16", "false").lower() in ('true', '1', 't')
-        
-        # Batching config (without dynamic_batching flag)
         self.kokoro_max_batch_size = int(os.getenv("KOKORO_MAX_BATCH_SIZE", "32"))
         self.kokoro_max_tokens_per_chunk = int(os.getenv("KOKORO_MAX_TOKENS_PER_CHUNK", "50"))
